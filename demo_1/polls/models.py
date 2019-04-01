0bcf86b60a474cd8dc49e5bd2489b37ffd42940a
zhangbin <zhangbin.email@foxmail.com>
2019-04-01 16:44:13 +0800

    second

--- demo_1/polls/models.py
+++ demo_1/polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_data <= now
         # return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_data'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
