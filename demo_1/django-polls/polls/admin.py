2ce9430b92c853ab597431d3f8109cea37633001
zhangbin <zhangbin.email@foxmail.com>
2019-04-03 17:11:16 +0800

    add package

---
+++ demo_1/django-polls/polls/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_data'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_data', 'was_published_recently')
+    list_filter = ['pub_data']
+
+admin.site.register(Question, QuestionAdmin)
